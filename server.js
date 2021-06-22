5c6e482b8d2b4a8ef2fe0e463d7ec8339117e7d4
palakjain21 <21palak2001@gmail.com>
2021-06-22 18:01:41 +0530

    Fixes

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/profile/:id', (req, res) => {
   profile.handleProfileGet(req, res, db);
 });
 
-app.listen( 3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen( process.env.PORT||3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT} `);
 });
 
